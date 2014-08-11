1988c7dfc4351d4a3b44438ed829a8bb9acb2612
Alejandro Sanchez Betancourt <alejandrolsca@gmail.com>
2014-08-11 02:47:59 -0500

    validation added to input field

--- dist/js/ng-fieldBuilder.js
+++ dist/js/ng-fieldBuilder.js
@@ -1,26 +1,62 @@
 (function(angular){
     
     angular.module('ggForm',[])
+        
+        .constant('validTypes',{
+            "singleSpaces": /^[^ \t\s]?([-_a-zA-Z0-9ÁáÉéÍíÓóÚú\.](.[^ \t\s])*)*[^ \t\s]?$/,
+            "rfc": /^[A-Za-z]{4}\-\d{6}(?:\-[A-Za-z\d]{3})?$/,
+            "email": /^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*$/i,
+            "decimal2": /^(\d+)?(\.\d{2,2})?$/,
+            "integer": /^\d$/,
+            "zipcode": /^\d{1,5}$/,
+            "date": /^\d{4}-\d{2}-\d{2}$/,
+            "user": /^\w{4,16}$/,
+            "password": /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$/
+        })
+            
 
-        .directive('ggInput',function(){
-            console.log('in');
+        .directive('ggInput',function(validTypes){
             return {
                     restrict: "E",
+                    require: '^ngModel',
                     scope: {
+                        widthLabel: '@',
+                        widthField: '@',
+                        label: '@',
                         type: '@',
-                        val: '='
+                        val: '=',
+                        ngModel: '='
                     },
                     template: '\
                                 <div class="form-group">\
-                                    <div class="col-lg-1">\
-                                        <input type="{{type}}" value="{{val}}" />\
+                                    <label for="{{name}}" class="{{widthLabel}} control-label">{{label}}</label>\
+                                    <div class="{{widthField}}">\
+                                        <input class="form-control" type="{{type}}" value="val" ng-model="ngModel" />\
+                                        <p ng-show="required">Required</p> \
+                                        <p ng-show="invalid">Invalid Value</p> \
                                     </div>\
-                                </div>'
+                                </div>',
+                    link: function(scope, elem, attrs, ctrl){
+                        ctrl.$parsers.unshift(validate);
+                        ctrl.$formatters.unshift(validate);
+                
+                        function validate(viewValue){
+                
+                            var required = ((attrs.req==="true") && !!!viewValue);
+                            var invalid = (angular.isDefined(viewValue) && !validTypes[attrs.valid].test(viewValue));
+                            
+                            scope.required = required;
+                            scope.invalid = invalid;
+                            
+                            ctrl.$setValidity('valid', ((required || invalid) ? false : true))
+                            
+                            return viewValue;
+                        }
+                    }
             };
         })
     
-        .directive('ggSelect',function(){
-            console.log('in');
+        .directive('ggSelect',function(validTypes){
             return {
                     restrict: "E",
                     scope: {
@@ -28,14 +64,14 @@
                         widthField: '@',
                         label: '@',
                         name: '@',
-                        options: '='
+                        options: '=',
+                        ngModel: '='
                     },
                     template: '\
                                 <div class="form-group">\
-                                    <label for="{{name}}" class="{{widthLabel}} control-label">{{label}}</label>\
-                                    <div class="widthField">\
-                                        <select class="form-control" id="{{name}}" name="{{name}}">\
-                                            <option ng-repeat="item in options" value="{{item.value}}">{{item.label}}</option>\
+                                    <label for="name" class="{{widthLabel}} control-label">{{label}}</label>\
+                                    <div class="{{widthField}}">\
+                                        <select class="form-control" id="{{name}}" name="{{name}}" ng-model="ngModel" ng-options="item.value as item.label for item in options">\
                                         </select>\
                                     </div>\
                                 </div>'
--- src/ng-fieldBuilder.js
+++ src/ng-fieldBuilder.js
@@ -1,11 +1,24 @@
 (function(angular){
     
     angular.module('ggForm',[])
+        
+        .constant('validTypes',{
+            "singleSpaces": /^[^ \t\s]?([-_a-zA-Z0-9ÁáÉéÍíÓóÚú\.](.[^ \t\s])*)*[^ \t\s]?$/,
+            "rfc": /^[A-Za-z]{4}\-\d{6}(?:\-[A-Za-z\d]{3})?$/,
+            "email": /^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*$/i,
+            "decimal2": /^(\d+)?(\.\d{2,2})?$/,
+            "integer": /^\d$/,
+            "zipcode": /^\d{1,5}$/,
+            "date": /^\d{4}-\d{2}-\d{2}$/,
+            "user": /^\w{4,16}$/,
+            "password": /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$/
+        })
+            
 
-        .directive('ggInput',function(){
-            console.log('in');
+        .directive('ggInput',function(validTypes){
             return {
                     restrict: "E",
+                    require: '^ngModel',
                     scope: {
                         widthLabel: '@',
                         widthField: '@',
@@ -19,13 +32,31 @@
                                     <label for="{{name}}" class="{{widthLabel}} control-label">{{label}}</label>\
                                     <div class="{{widthField}}">\
                                         <input class="form-control" type="{{type}}" value="val" ng-model="ngModel" />\
+                                        <p ng-show="required">Required</p> \
+                                        <p ng-show="invalid">Invalid Value</p> \
                                     </div>\
-                                </div>'
+                                </div>',
+                    link: function(scope, elem, attrs, ctrl){
+                        ctrl.$parsers.unshift(validate);
+                        ctrl.$formatters.unshift(validate);
+                
+                        function validate(viewValue){
+                
+                            var required = ((attrs.req==="true") && !!!viewValue);
+                            var invalid = (angular.isDefined(viewValue) && !validTypes[attrs.valid].test(viewValue));
+                            
+                            scope.required = required;
+                            scope.invalid = invalid;
+                            
+                            ctrl.$setValidity('valid', ((required || invalid) ? false : true))
+                            
+                            return viewValue;
+                        }
+                    }
             };
         })
     
-        .directive('ggSelect',function(){
-            console.log('in');
+        .directive('ggSelect',function(validTypes){
             return {
                     restrict: "E",
                     scope: {
